eb520a787e26f9e05d8e7132f3cc5dcfcf4d62c6
Oksana19761219 <o.valioniene@gmail.com>
2022-04-05 20:48:13 +0300

    61 uzduotis FizzBuzz ideta

---
+++ 61_JS_FizzBuzz/fizz_buzz.js
@@ -0,0 +1,20 @@
+function playFizzBuzz (number) {
+    let resultArray = []
+    let result;
+    for (let i = 1; i <= number; i++) {
+        if (i % 15 === 0) {
+            result = "fizz buzz";
+        } else if (i % 5 === 0) {
+            result = "buzz";
+        } else if (i % 3 === 0) {
+            result = "fizz";
+        } else {result = i};
+        resultArray.push(result);
+    };
+    return resultArray;
+};
+
+let fizzBuzz40 = playFizzBuzz(40);
+fizzBuzz40
+
+
